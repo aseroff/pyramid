924155692b1760857a75226c970cb7055ff805aa
Andrew Seroff <andy@seroff.co>
2019-03-01 14:39:48 -0600

    Update Pyramid_StreamlabsSystem.py

--- Pyramid_StreamlabsSystem.py
+++ Pyramid_StreamlabsSystem.py
@@ -50,12 +50,11 @@ def Init():
 			"rewardNineWide": 9,
 			"rewardTenPlusWide": 10,
 		}
-		
+
 def Execute(data):
 	global msg, user, count, width, desc, settings
 	if ((settings["liveOnly"] and Parent.IsLive()) or (not settings["liveOnly"])) and data.IsChatMessage():
 		Parent.Log("pyramids",count)
-		void Log(string "pyramid", string count + " " + msg)
 		if ((count == 0) and (len(data.Message.strip().split(" ")) == 1)):
 			user = data.UserName
 			msg = data.Message.strip().split(" ")[0]
