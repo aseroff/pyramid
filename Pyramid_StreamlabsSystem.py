3309a4b86d6c12293da104874f8e7803746748d2
andy seroff <aseroff@umn.edu>
2020-04-24 18:31:09 -0500

    merges refactored code to master

--- Pyramid_StreamlabsSystem.py
+++ Pyramid_StreamlabsSystem.py
@@ -7,149 +7,158 @@ ScriptName = "Emote Pyramid"
 Website = "https://github.com/aseroff/pyramid/"
 Description = "Congratulate users on completion of emote pyramids"
 Creator = "rvaen17"
-Version = "1.2.0"
+Version = "1.2.1"
 
-configFile = "config.json"
-settings = {}
-msg = ""
-user = ""
-count = 0
-width = 0
-desc = 0
+CONFIG = "config.json"
+SETTINGS = {}
+MSG = ""
+USER = ""
+COUNT = 0
+WIDTH = 0
+MAXIMUM = 0
+DESC = False
+
 
 def ScriptToggled(state):
-	return
+    return
+
 
 def Init():
-	global configFile, settings
+    global CONFIG, SETTINGS
 
-	path = os.path.dirname(__file__)
-	try:
-		with codecs.open(os.path.join(path, configFile), encoding='utf-8-sig', mode='r') as file:
-			settings = json.load(file, encoding='utf-8-sig')
-	except:
-		settings = {
-			"liveOnly": False,
-			"multiUser": False,
-			"responseBlocked": "/me $user with the block! Kappa",
-			"responseChoked": "/me $user managed to mess up their own pyramid. NotLikeThis",
-			"responseThreeWide": "/me $user just finished a 3-wide $emote pyramid! Pretty good TPFufun",
-			"responseFourWide": "/me $user just finished a 4-wide $emote pyramid! Nice LUL",
-			"responseFiveWide": "/me $user just finished a 5-wide $emote pyramid! Very nice MVGame",
-			"responseSixWide": "/me $user just finished a 6-wide $emote pyramid! Wow PogChamp",
-			"responseSevenWide": "/me $user just finished a 7-wide $emote pyramid! Incredible KAPOW",
-			"responseEightWide": "/me $user just finished a 8-wide $emote pyramid! Crazy OhMyDog",
-			"responseNineWide": "/me $user just finished a 9-wide $emote pyramid! Insane SwiftRage",
-			"responseTenPlusWide": "/me $user just finished a 10+ $emote pyramid! Impossible Kreygasm",
-			"rewardThreeWide": 0,
-			"rewardFourWide": 0,
-			"rewardFiveWide": 5,
-			"rewardSixWide": 6,
-			"rewardSevenWide": 7,
-			"rewardEightWide": 8,
-			"rewardNineWide": 9,
-			"rewardTenPlusWide": 10,
-		}
+    path = os.path.dirname(__file__)
+    try:
+        with codecs.open(os.path.join(path, CONFIG), encoding='utf-8-sig', mode='r') as file:
+            SETTINGS = json.load(file, encoding='utf-8-sig')
+    except:
+        SETTINGS = {
+            "liveOnly": False,
+            "multiUser": False,
+            "responseBlocked": "/me $user with the block! Kappa",
+            "responseChoked": "/me $user managed to mess up their own pyramid. NotLikeThis",
+            "responseThreeWide": "/me $user just finished a 3-wide $emote pyramid! Pretty good TPFufun",
+            "responseFourWide": "/me $user just finished a 4-wide $emote pyramid! Nice LUL",
+            "responseFiveWide": "/me $user just finished a 5-wide $emote pyramid! Very nice MVGame",
+            "responseSixWide": "/me $user just finished a 6-wide $emote pyramid! Wow PogChamp",
+            "responseSevenWide": "/me $user just finished a 7-wide $emote pyramid! Incredible KAPOW",
+            "responseEightWide": "/me $user just finished a 8-wide $emote pyramid! Crazy OhMyDog",
+            "responseNineWide": "/me $user just finished a 9-wide $emote pyramid! Insane SwiftRage",
+            "responseTenPlusWide": "/me $user just finished a 10+ $emote pyramid! Impossible Kreygasm",
+            "rewardThreeWide": 0,
+            "rewardFourWide": 0,
+            "rewardFiveWide": 5,
+            "rewardSixWide": 6,
+            "rewardSevenWide": 7,
+            "rewardEightWide": 8,
+            "rewardNineWide": 9,
+            "rewardTenPlusWide": 10,
+        }
+
 
 def Execute(data):
-	global msg, user, count, width, desc, settings, reset
-	if ((settings["liveOnly"] and Parent.IsLive()) or (not settings["liveOnly"])) and data.IsChatMessage():
-		if ((count == 0) and (len(data.Message.strip().split(" ")) == 1)):
-			user = data.UserName
-			msg = data.Message.strip().split(" ")[0]
-			count = 1
-			desc = 0
-		elif (count > 0):
-			if ((data.UserName == user) or settings["multiUser"]):
-				if (desc == 0) and (len(data.Message.strip().split(" ")) == (count + 1)) and (data.Message.strip().split(" ")[0] == msg) and (len(list(set(data.Message.strip().split(" ")))) == 1):
-					count += 1
-					width = count
-				elif (len(data.Message.strip().split(" ")) == (count - 1)) and (data.Message.split(" ")[0] == msg) and (len(list(set(data.Message.split(" ")))) == 1):
-					count -= 1
-					desc = 1
-					if count == 1:
-						if width == 3:
-							outputMessage = settings["responseThreeWide"]
-							reward = settings["rewardThreeWide"]
-						elif width == 4:
-							outputMessage = settings["responseFourWide"]
-							reward = settings["rewardFourWide"]
-						elif width == 5:
-							outputMessage = settings["responseFiveWide"]
-							reward = settings["rewardFiveWide"]
-						elif width == 6:
-							outputMessage = settings["responseSixWide"]
-							reward = settings["rewardSixWide"]
-						elif width == 7:
-							outputMessage = settings["responseSevenWide"]
-							reward = settings["rewardSevenWide"]
-						elif width == 8:
-							outputMessage = settings["responseEightWide"]
-							reward = settings["rewardEightWide"]
-						elif width == 9:
-							outputMessage = settings["responseNineWide"]
-							reward = settings["rewardNineWide"]
-						elif width > 9:
-							outputMessage = settings["responseTenPlusWide"]
-							reward = settings["rewardTenPlusWide"]
-						if width > 2:
-							outputMessage = outputMessage.replace("$user", data.UserName)
-							outputMessage = outputMessage.replace("$emote", msg)
-							Parent.SendStreamMessage(outputMessage)
-							Parent.AddPoints(data.User,data.UserName,reward)
-						reset()
-				elif (count > 1):
-					if (data.UserName == user):
-						Parent.SendStreamMessage(settings["responseChoked"].replace("$user", data.UserName))
-					else:
-						Parent.SendStreamMessage(settings["responseBlocked"].replace("$user", data.UserName))
-					user = ""
-					msg = ""
-					count = 0
-					width = 0
-					desc = 0
-				else:
-					if (len(data.Message.strip().split(" ")) == 1):
-						user = data.UserName
-						msg = data.Message.strip().split(" ")[0]
-						count = 1
-						desc = 0
-					else:
-						reset()
-			elif (count > 1):
-				Parent.SendStreamMessage(settings["responseBlocked"].replace("$user", data.UserName))
-				user = ""
-				msg = ""
-				count = 0
-				width = 0
-				desc = 0
-			else:
-				if (len(data.Message.strip().split(" ")) == 1):
-					user = data.UserName
-					msg = data.Message.strip().split(" ")[0]
-					count = 1
-					desc = 0
-				else:
-					reset()
-		else:
-			reset()
-	return
+    global MSG, USER, COUNT, WIDTH, MAXIMUM, DESC, SETTINGS
+    if ((SETTINGS["liveOnly"] and Parent.IsLive()) or (not SETTINGS["liveOnly"])) and data.IsChatMessage():
+        if WIDTH > 0:
+            if ((data.UserName == USER) or SETTINGS["multiUser"]):
+                set_count(data.Message, data.UserName)
+                if (DESC == False) and (COUNT == (WIDTH + 1)):
+                    WIDTH = COUNT
+                    MAXIMUM = WIDTH
+                elif COUNT == (WIDTH - 1):
+                    WIDTH -= 1
+                    DESC = True
+                    if WIDTH == 1:
+                        if MAXIMUM > 2:
+                            Parent.SendStreamMessage(complete_message(MAXIMUM).replace("$user", USER).replace("$emote", MSG))
+                            Parent.AddPoints(data.User, data.UserName, complete_reward(MAXIMUM))
+                        reset()
+                else:
+                    if data.UserName == USER:
+                        Parent.SendStreamMessage(SETTINGS["responseChoked"].replace("$user", data.UserName))
+                    else:
+                        Parent.SendStreamMessage(SETTINGS["responseBlocked"].replace("$user", data.UserName))
+                    reset()
+            elif (WIDTH > 1 or ((WIDTH == 1) and DESC)):
+                Parent.SendStreamMessage(SETTINGS["responseBlocked"].replace("$user", data.UserName))
+                reset()
+        else:
+            reset(data.Message, data.UserName)
+    return
 
-def reset():
-	user = ""
-	msg = ""
-	count = 0
-	width = 0
-	desc = 0
+
+def set_count(message, username):
+    global MSG, COUNT
+    split = message.strip().split(" ")
+    if (len(list(set(split))) == 1) and (split[0] == MSG) and (split[0] != ""):
+        COUNT = len(split)
+    else:
+        reset(message, username)
+    return
+
+
+def reset(message="", username=""):
+    global MSG, USER, COUNT, WIDTH, DESC
+    if message != "":
+        split = message.strip().split(" ")
+        if len(split) == 1:
+            MSG = split[0]
+            USER = username
+            COUNT = 1
+            WIDTH = 1
+            DESC = False
+    else:
+        MSG = ""
+        USER = ""
+        COUNT = 0
+        WIDTH = 0
+        DESC = False
+    return
+
+
+def complete_message(pyramid_width):
+    global SETTINGS
+    message = ""
+    if pyramid_width >= 3:
+        switcher = {
+            3: SETTINGS["responseThreeWide"],
+            4: SETTINGS["responseFourWide"],
+            5: SETTINGS["responseFiveWide"],
+            6: SETTINGS["responseSixWide"],
+            7: SETTINGS["responseSevenWide"],
+            8: SETTINGS["responseEightWide"],
+            9: SETTINGS["responseNineWide"]
+        }
+        message = switcher.get(pyramid_width) or SETTINGS["responseTenPlusWide"]
+    return message
+
+
+def complete_reward(pyramid_width):
+    global SETTINGS
+    value = 0
+    if pyramid_width >= 3:
+        switcher = {
+            3: SETTINGS["rewardThreeWide"],
+            4: SETTINGS["rewardFourWide"],
+            5: SETTINGS["rewardFiveWide"],
+            6: SETTINGS["rewardSixWide"],
+            7: SETTINGS["rewardSevenWide"],
+            8: SETTINGS["rewardEightWide"],
+            9: SETTINGS["rewardNineWide"]
+        }
+        value = switcher.get(pyramid_width) or SETTINGS["rewardTenPlusWide"]
+    return value
+
 
 def ReloadSettings(jsonData):
-        Init()
-	return
+    Init()
+    return
+
 
 def OpenReadMe():
-	location = os.path.join(os.path.dirname(__file__), "README.txt")
-	os.startfile(location)
-	return
+    location = os.path.join(os.path.dirname(__file__), "README.txt")
+    os.startfile(location)
+    return
+
 
 def Tick():
-	return
+    return
